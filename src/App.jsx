0816fb54d4c52d538cca40d0392cd629ba5e05db
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-05-11 20:34:33 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello from App Component</h1>
+      <p>This is your Vite + React app running!</p>
+    </div>
+  );
+};
+
+export default App;
